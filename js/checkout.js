060058547222240530e8ceee110d78e0ff6fb628
ScaferuZ <endrafruz@gmail.com>
2023-06-28 14:55:25 +0700

    feat: done working on checkout screen

--- js/checkout.js
+++ js/checkout.js
@@ -7,3 +7,24 @@ tl.from("#check_head", { autoAlpha: 0, y: -50, duration: 0.5 })
   .from("#shipping_detail", { autoAlpha: 0, duration: 0.5 })
   .from("#ttl_check", { y: 50, autoAlpha: 0, duration: 0.5 })
   .from("#pay_btn", { y: 50, autoAlpha: 0, duration: 0.5 });
+
+const btn = document.getElementById("pay_btn");
+const menu = document.getElementById("popup-menu");
+const content2 = document.getElementById("content2");
+
+function openMenu() {
+  menu.classList.toggle("hidden");
+  // gsap.from("#popup-menu", { y: -50, autoAlpha: 0, duration: 0.5 });
+}
+
+function closeMenu() {
+  window.location.href = "cart.html";
+}
+
+const backdrop = document.getElementById("popup-backdrop");
+const backdrop2 = document.getElementById("popup-backdrop2");
+
+function removeContent() {
+  content2.remove();
+  console.log("awok");
+}
