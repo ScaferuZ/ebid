d2151e6b28c04344fd0a3bbd73170138f4476dfd
ScaferuZ <endrafruz@gmail.com>
2023-07-12 17:50:27 +0700

    add a little change including navbar padding and checkout drawer

--- js/checkout.js
+++ js/checkout.js
@@ -9,8 +9,9 @@ tl.from("#check_head", { autoAlpha: 0, y: -50, duration: 0.5 })
   .from("#pay_btn", { y: 50, autoAlpha: 0, duration: 0.5 });
 
 const btn = document.getElementById("pay_btn");
-const menu = document.getElementById("popup-menu");
+const menu = document.getElementById("drawer-bottom-example");
 const content2 = document.getElementById("content2");
+const content1 = document.getElementById("content1");
 
 function openMenu() {
   menu.classList.toggle("hidden");
@@ -26,5 +27,6 @@ const backdrop2 = document.getElementById("popup-backdrop2");
 
 function removeContent() {
   content2.remove();
+  content1.classList.remove("hidden");
   console.log("awok");
 }
